728a5723f23209800df8e13001a568dac385fd54
menesdev <mustafaguneruz19@gmail.com>
2023-07-01 12:57:49 +0300

    Open the clicked section

---
+++ Project5-Flexbox-Panel-Gallery/script.js
@@ -0,0 +1,8 @@
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+  this.classList.toggle('open');
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen))
+
