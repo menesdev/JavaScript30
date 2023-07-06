cfbe9daccb9dfd379ca8d815ca348900d013d3e1
menesdev <mustafaguneruz19@gmail.com>
2023-07-06 10:31:10 +0300

    Done

--- Project9-Dev-Tools-Tricks/script.js
+++ Project9-Dev-Tools-Tricks/script.js
@@ -7,25 +7,58 @@ function makeGreen() {
 }
 
 // Regular
+console.log('hello');
 
 // Interpolated
+console.log('Hello I am a %s string!', '🌸');
 
 // Styled
+// console.log('%c I am some great text', 'font-size:50px; background: red;')
 
 // warning!
+console.warn('OH NOOO');
 
 // Error :|
+console.error('I am an error');
 
 // Info
+console.info('Crocodiles eat 3-4 people per year')
 
 // Testing
+console.assert(1 === 2, 'Thats wrong')
 
 // clearing
+console.clear();
 
 // Viewing DOM Elements
+// console.log(p);
+// console.dir(p);
+
+console.clear();
 
 // Grouping together
+dogs.forEach(dog => {
+  console.group(`${dog.name}`)
+  console.log(`This is ${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old`)
+  console.log(`${dog.name} is ${dog.age * 7} dog years old`)
+  console.groupEnd(`${dog.name}`)
+})
 
 // counting
+console.count('Wes')
+console.count('Wes')
+console.count('Wes')
+console.count('Wes')
 
 // timing
+console.time('fetching data')
+fetch('https://api.github.com/users/menesdev')
+  .then(data => data.json())
+  .then(data => {
+    console.timeEnd('fetching data')
+    console.log(data)
+  })
+
+//table
+console.table(dogs)
