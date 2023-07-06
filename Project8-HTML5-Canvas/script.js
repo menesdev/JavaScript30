d5760f8f2db22e6cd7facb07a8da78dc41c861e6
menesdev <mustafaguneruz19@gmail.com>
2023-07-06 09:56:48 +0300

    Done!

--- Project8-HTML5-Canvas/script.js
+++ Project8-HTML5-Canvas/script.js
@@ -10,10 +10,13 @@ ctx.lineCap = 'round';
 let isDrawing = false;
 let lastX= 0;
 let lastY= 0;
+let hue = 0;
+let direction = true;
 
 function draw(e) {
   if(!isDrawing) return; //stop the function from running when they are not moused.
   console.log(e);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
   //start from
   ctx.moveTo(lastX, lastY);
@@ -22,6 +25,20 @@ function draw(e) {
   ctx.stroke();
   [lastX, lastY] = [e.offsetX, e.offsetY];
   
+  hue++;
+  if(hue >= 360) {
+    hue = 0;
+  }
+
+  if(ctx.lineWidth >= 90 || ctx.lineWidth <= 1 ) {
+    direction = !direction;
+  }
+
+  if(direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
 }
 
 canvas.addEventListener('mousedown', (e) => {
